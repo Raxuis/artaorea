11be08b0f5c55717dee7dc6639e1b09c96f8f466
Raxuis <raclot.raphael@gmail.com>
2024-08-02 19:09:29 +0200

    feat(about-me page): :sparkles: add event on shapes hover

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -34,22 +34,22 @@ const AboutMe = () => {
         <StudiesTimeline />
         <p className="text-xl max-sm:text-center">Mes centres d'intérêts</p>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-10 place-items-center mt-5">
-          <div className="size-60 bg-shape-1 bg-no-repeat bg-center bg-cover flex justify-center items-center">
+          <div className="size-60 bg-shape-1 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-2  duration-500">
             <p className="text-sm">Escalade</p>
           </div>
-          <div className="size-60 bg-shape-2 bg-no-repeat bg-center bg-cover flex justify-center items-center">
-            <p className="text-sm">Pâtisserie et cuisine</p>
+          <div className="size-60 bg-shape-2 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-1 duration-500 group">
+            <p className="text-sm group-hover:pl-4 duration-500">Pâtisserie et cuisine</p>
           </div>
-          <div className="size-60 bg-shape-1 bg-no-repeat bg-center bg-cover flex justify-center items-center">
+          <div className="size-60 bg-shape-1 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-2 duration-500">
             <p className="text-sm">Art</p>
           </div>
-          <div className="size-60 bg-shape-2 rotate-90 bg-no-repeat bg-center bg-cover flex justify-center items-center">
+          <div className="size-60 bg-shape-2 rotate-90 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-1 duration-500">
             <p className="text-sm -rotate-90">Voyages</p>
           </div>
-          <div className="size-60 bg-shape-1 rotate-180 bg-no-repeat bg-center bg-cover flex justify-center items-center">
+          <div className="size-60 bg-shape-1 rotate-180 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-2 duration-500">
             <p className="text-sm -rotate-180 pr-4">Jeux de société</p>
           </div>
-          <div className="size-60 bg-shape-2 rotate-90 bg-no-repeat bg-center bg-cover flex justify-center items-center">
+          <div className="size-60 bg-shape-2 rotate-90 bg-no-repeat bg-center bg-cover flex justify-center items-center hover:bg-shape-1 duration-500">
             <p className="text-sm -rotate-90 max-w-10">Poterie, dessin, perles</p>
           </div>
         </div>
