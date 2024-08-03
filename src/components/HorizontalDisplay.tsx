c5d11d52ccdd69ca750841df18b8b701b3931266
Raxuis <raclot.raphael@gmail.com>
2024-08-03 13:48:52 +0200

    feat(horizontalDisplay component): :sparkles: add possibility to add property reversed so the grid is reversed

--- src/components/HorizontalDisplay.tsx
+++ src/components/HorizontalDisplay.tsx
@@ -9,25 +9,47 @@ interface HorizontalDisplayProps {
   dimensions: string;
   technics: string[];
   description: string;
+  reversed?: boolean;
 }
 
-const HorizontalDisplay = ({ imageSrc, alt, className, title, dimensions, technics, description }: HorizontalDisplayProps) => {
+const HorizontalDisplay = ({ imageSrc, alt, className, title, dimensions, technics, description, reversed }: HorizontalDisplayProps) => {
   return (
-    <div className={`grid grid-cols-1 lg:grid-cols-[2fr_2fr] xl:grid-cols-[1fr_3fr] gap-[15vw] sm:gap-[10vw] art-container ${className}-container max-sm:m-5`}>
-      <img src={imageSrc} alt={alt} className={`${className} h-[500px] w-[400px] object-cover rounded-lg mx-auto`} />
-      <div className="art-text">
-        <div className={`art-header bg-cyan text-white ${className}-header w-full text-center p-3 sm:p-5 space-y-1`}>
-          <h1 className="border-b border-white inline-flex">{title}</h1>
-          <p>{dimensions}</p>
-          <p>Technique: {technics.join(', ')}</p>
-        </div>
-        <div className="art-content text-lg xl:text-xl mt-2">
-          <p>
-            {description}
-          </p>
-        </div>
-      </div>
-    </div>
+    <>
+      {
+        reversed ? (
+          <div className={`grid grid-cols-1 lg:grid-cols-[2fr_2fr] xl:grid-cols-[3fr_1fr] gap-[15vw] sm:gap-[10vw] art-container ${className}-container max-sm:m-5`}>
+            <div className="art-text">
+              <div className={`art-header bg-cyan text-white ${className}-header w-full text-center p-3 sm:p-5 space-y-1`}>
+                <h1 className="border-b border-white inline-flex">{title}</h1>
+                <p>{dimensions}</p>
+                <p>Technique: {technics.join(', ')}</p>
+              </div>
+              <div className="art-content text-lg xl:text-xl mt-2">
+                <p>
+                  {description}
+                </p>
+              </div>
+            </div>
+            <img src={imageSrc} alt={alt} className={`${className} h-[500px] w-[400px] object-cover rounded-lg mx-auto`} />
+          </div>
+        ) : (
+          <div className={`grid grid-cols-1 lg:grid-cols-[2fr_2fr] xl:grid-cols-[1fr_3fr] gap-[15vw] sm:gap-[10vw] art-container ${className}-container max-sm:m-5`}>
+            <img src={imageSrc} alt={alt} className={`${className} h-[500px] w-[400px] object-cover rounded-lg mx-auto`} />
+            <div className="art-text">
+              <div className={`art-header bg-cyan text-white ${className}-header w-full text-center p-3 sm:p-5 space-y-1`}>
+                <h1 className="border-b border-white inline-flex">{title}</h1>
+                <p>{dimensions}</p>
+                <p>Technique: {technics.join(', ')}</p>
+              </div>
+              <div className="art-content text-lg xl:text-xl mt-2">
+                <p>
+                  {description}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+    </>
   )
 }
 
